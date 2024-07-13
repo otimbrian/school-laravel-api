c1ae6346d656e72f5db828b422ae83aa9202b116
otimbrian <otims.brian@gmail.com>
2024-07-14 02:46:00 +0300

    Changed route

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,6 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::get('students', [StudentController::class, 'index']);
-Route::post('student', [StudentController::class, 'store']);
+Route::post('students', [StudentController::class, 'store']);
 Route::put('/students/{id}', [StudentController::class, 'update']);
 Route::delete('/students/{id}', [StudentController::class, 'destroy']);
